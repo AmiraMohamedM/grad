ff9fa13912d493b12c9717a852a478d906ab500d
Amira <amira.mo2004@gmail.com>
2026-02-23 19:26:50 +0200

    Added Home endpoint and updated Postman collection

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>()
     .AddDefaultTokenProviders();
 
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<HomeService>();
 
 // ------------------- JWT Authentication -------------------
 var jwtSection = builder.Configuration.GetSection("JwtSettings");
